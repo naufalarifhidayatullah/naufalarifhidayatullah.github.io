d1890aa029a5e7400e0ee5ac3191294c37250710
naufalarifhidayatullah <112938037+naufalarifhidayatullah@users.noreply.github.com>
2022-09-12 20:21:46 +0700

    Add files via upload

---
+++ p03jquery/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.add_new').click(function(){
+		alert('Tambah Anggota?')
+	})
+})
+
+$(document).ready(function(){
+	$('.edit').click(function(){
+		alert('Apakah Anda Ingin Mengeditnya?')
+	})
+})
+
+$(document).ready(function(){
+	$('.delet').click(function(){
+		alert('Apakah Anda Ingin Menghapusnya?')
+	})
+})
+
+$(document).ready(function(){
+	$('.foto').click(function(){
+		alert('Perbesar Foto?')
+	})
+})
